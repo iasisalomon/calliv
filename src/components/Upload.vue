9685c885e932848518724d48b5b20c75fb6c35e1
Iasi Salomon <iasisalomon@gmail.com>
2020-09-19 17:54:29 -0300

    asd

---
+++ src/components/Upload.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- Styled -->
+    <b-form-file
+      v-model="file1"
+      :state="Boolean(file1)"
+      placeholder="Choose a file or drop it here..."
+      drop-placeholder="Drop file here..."
+    ></b-form-file>
+    <button v-on:click="log"> asd </button>
+    <p>{{file1}}</p>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        file1: null,
+      }
+    },
+    methods: {
+        log: (e)=> console.log (e.target.file1[0])
+      }
+    }
+</script>
