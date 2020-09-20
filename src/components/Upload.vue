efa25f82df19e1ccde2114d7c4bb353c635b8568
Iasi Salomon <iasisalomon@gmail.com>
2020-09-20 14:29:28 -0300

    asd

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,28 +1,39 @@
 <template>
-  <div>
-    <!-- Styled -->
-    <b-form-file
-      v-model="file1"
-      :state="Boolean(file1)"
-      placeholder="Choose a file or drop it here..."
-      drop-placeholder="Drop file here..."
-    ></b-form-file>
-    <button v-on:click="log"> asd </button>
-    <p>{{file1}}</p>
+<div class="row">
+  <div class="row">
+      <p>{{csvdata}}</p>
+</div>
+<div class="input-group mb-3">
+  <div class="custom-file">
+    <input type="file" @change="uploadFile" class="custom-file-input" id="inputGroupFile02">
+    <label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
+    
   </div>
+  <div class="input-group-append">
+    <span class="input-group-text" id="inputGroupFileAddon02">Upload</span>
+  </div>
+</div>
+</div>
 </template>
-
 <script>
-
+import Papa from 'papaparse';
 
   export default {
     data() {
       return {
-        file1: null,
+        csvdata:[],
       }
     },
     methods: {
-        log: (e)=> console.log (e.target.file1[0])
+      uploadFile(e){
+        let file = e.target.files[0];
+        Papa.parse(file, {
+	      complete: function(results) {
+        console.log(results);
+        this.csvdata = results;
+	      }
+        });
+      }
       }
     }
 </script>
