51233291eccc077f3169a489c9f09bbe3ce833d4
Iasi Salomon <iasisalomon@gmail.com>
2020-09-22 13:54:36 -0300

    asd

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -11,6 +11,12 @@
 import Navbar from "../components/Navbar"
 import Upload from "../components/Upload"
 export default {
+      data() {
+      return {
+        csvdata:[],
+        filename:"Choose file"
+      }
+    },
   components: {
     Navbar,
     Upload 
