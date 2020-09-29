dec40a2b5e525c48e9760ed284100c02c3223722
Iasi Salomon <iasisalomon@gmail.com>
2020-09-29 01:13:26 -0300

    gracias renkon sos lo mas

--- src/views/Procesados.vue
+++ src/views/Procesados.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
     <Navbar />
-     <Nav />
-  <div>
-    {{condensedWells}}
-  </div>
-    <ProcessedTable :csvdata="csvdata" :filename="filename" :tableConfig="tableConfig" />
+    <Nav />
+    <div>
+      {{ csvdata }}
+    </div>
+    <div>
+      {{ adjustedResult }}
+    </div>
+    <ProcessedTable
+      :csvdata="csvdata"
+      :filename="filename"
+      :tableConfig="tableConfig"
+    />
   </div>
 </template>
 
@@ -14,30 +21,42 @@ import Navbar from "../components/Navbar";
 import Nav from "../components/Nav";
 import ProcessedTable from "../components/ProcessedTable";
 
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
+
 export default {
   data() {
     return {
       csvdata: [],
       filename: [],
-      tableConfig:[],
-      condensedWells:[],
+      tableConfig: [],
+      condensedWells: [],
+      adjustedResult: [],
     };
   },
   components: {
     Navbar,
     ProcessedTable,
-    Nav
+    Nav,
   },
   methods: {
+
   },
-  created(){
-    this.filename = this.$route.params.data[0]
-    this.tableConfig = this.$route.params.data[1]
-    this.csvdata = this.$route.params.data[2].sort()
-    this.condensedWells = this.csvdata.map((row)=>{
-      return row[2]
-    })
-  }
+  created() {
+    this.filename = this.$route.params.data[0];
+    this.tableConfig = this.$route.params.data[1];
+    this.csvdata = this.$route.params.data[2].sort();
+    this.condensedWells = this.csvdata.map((row) => {
+      return row[0];
+    });
+    this.condensedWells = [...new Set(this.condensedWells)];
+    this.adjustedResult = groupBy(this.csvdata, 0);
+  },
 };
 </script>
 
