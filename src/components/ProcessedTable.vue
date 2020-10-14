5c77648424bc10c275a4e00c79d00681f185c0ff
Iasi Salomon <iasisalomon@gmail.com>
2020-10-14 17:04:56 -0300

    removido flag

--- src/components/ProcessedTable.vue
+++ src/components/ProcessedTable.vue
@@ -1,8 +1,6 @@
 <template>
   <table class="table table-striped">
     <thead>
-      <div> {{ tableConfig }} </div>
-      <div> {{ csvdata }} </div>
       <tr>
         <th v-for="(header, ind) in this.tableConfig" :key="ind" scope="col">
           {{ header }}
