0c669a2013b22ca6b8555d9f3168057710ef11c4
Iasi Salomon <iasisalomon@gmail.com>
2020-10-25 20:42:41 -0300

    curvas nav

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import("../views/Procesados.vue")
+  },
+  {
+    path: "/curvas",
+    name: "Curvas",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/Curvas.vue")
   }
 ];
 
