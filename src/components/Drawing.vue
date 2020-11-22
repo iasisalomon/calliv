bd87406232fb20e458de620c8f499258eda6efc7
Iasi Salomon <iasisalomon@gmail.com>
2020-11-22 18:01:59 -0300

    yolo

--- src/components/Drawing.vue
+++ src/components/Drawing.vue
@@ -6,7 +6,7 @@
 <script>
 import * as tf from "@tensorflow/tfjs";
 import p5 from "../lib/p5.min.js";
-import * from "../script/linearRegression";
+import linear from "../script/linearRegression";
 
 export default {
   props: {},
