0ff1e5d9c1b9d685aa38089f0871798e5d97f534
Iasi Salomon <iasisalomon@gmail.com>
2020-11-22 18:00:34 -0300

    asd

--- src/components/Drawing.vue
+++ src/components/Drawing.vue
@@ -1,12 +1,18 @@
 <template>
+  <div class="container">
+      <body></body>   
+  </div>
 </template>
 <script>
+import * as tf from "@tensorflow/tfjs";
+import p5 from "../lib/p5.min.js";
+import * from "../script/linearRegression";
 
 export default {
   props: {},
   data() {
     return {};
   },
-  methods: {}
+  methods: {},
 };
 </script>
--- src/components/FitSelector.vue
+++ src/components/FitSelector.vue
@@ -2,13 +2,17 @@
   <form>
     <div class="form-row">
       <div class="col-7">
-        <input type="text" class="form-control" placeholder="City" />
+        <input type="text" class="form-control" placeholder="Tipo de Curva" />
       </div>
       <div class="col">
-        <input type="text" class="form-control" placeholder="State" />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Grado del Polinomio"
+        />
       </div>
       <div class="col">
-        <input type="text" class="form-control" placeholder="Zip" />
+        <input type="text" class="form-control" placeholder="Queremos flan" />
       </div>
     </div>
   </form>
