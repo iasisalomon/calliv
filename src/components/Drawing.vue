d6000b0cbe9c3901a6b1de24253d8df46be274a3
Iasi Salomon <iasisalomon@gmail.com>
2020-11-23 18:06:58 -0300

    batiendo fruta

--- src/components/Drawing.vue
+++ src/components/Drawing.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container mt-1">
+    {{ xs }}
     {{ ys }}
     <div id="canvas" class="row justify-content-center"></div>
   </div>
@@ -15,27 +16,34 @@ export default {
     return {
       curveBy4: [],
       ys: [],
+      xs: [0, 4.2, 9.7, 14.6, 22, 33.3, 50, 100],
     };
   },
   methods: {},
   created() {
+    localStorage.setItem("xs", JSON.stringify(this.xs));
     const sketch = (s) => {
       let w = 800;
       let h = 500;
 
-      function grid() {
-        for (var x = 0; x < w; x += 20) {
-          s.line(x, 0, x, h);
-          for (var y = 0; y < h; y += 20) {
-            s.line(0, y, w, y);
-          }
-        }
-      }
+      // function grid() {
+      //   for (var x = 0; x < w; x += 20) {
+      //     s.line(x, 0, x, h);
+      //     for (var y = 0; y < h; y += 20) {
+      //       s.line(0, y, w, y);
+      //     }
+      //   }
+      //   s.translate(0, h);
+      //   s.scale(1, -1);
+      // }
 
-      function displayMousePosition() {
-        s.rect(0, 0, 60, 20);
-        s.text(s.mouseX + "," + s.mouseY, 10, 15);
-      }
+      // function displayMousePosition() {
+      //   s.push();
+      //   s.rect(0, 480, 60, 20);
+      //   s.scale(1, -1);
+      //   s.text(s.mouseX + "," + s.mouseY, 5, -485);
+      //   s.pop();
+      // }
 
       s.setup = () => {
         s.createCanvas(w, h);
@@ -43,10 +51,15 @@ export default {
 
       s.draw = () => {
         s.background(220);
-        grid();
-        displayMousePosition();
+        // grid();
+        // displayMousePosition();
+        s.push();
+        s.stroke("purple"); // Change the color
+        s.strokeWeight(10); // Make the points 10 pixels in size
+        s.pop();
       };
     };
+
     new P5(sketch, "canvas");
     this.curveBy4 = JSON.parse(localStorage.getItem("curveBy4"));
     this.ys = this.curveBy4.map((el) => {
