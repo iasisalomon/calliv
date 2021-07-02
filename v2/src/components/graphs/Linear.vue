ab884380bb394487de15cebdfd9bc5251028e4e5
Iasi Salomon <iasi@scalecoding.com>
2021-07-02 17:54:47 -0300

    vue vue

---
+++ v2/src/components/graphs/Linear.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters('data', [
+      'matrixZero',
+      'getMinimumMeasureNoise',
+      'wellRowsValues',
+    ]),
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
