103b828c2905ef2fe233135c9d89e1e17ac8b822
Iasi Salomon <iasi@scalecoding.com>
2021-08-12 00:30:26 -0300

    fit linear advance

--- v2/src/plugins/fitLinear.js
+++ v2/src/plugins/fitLinear.js
@@ -1,18 +1,25 @@
-import * as tf from '@tensorflow/tfjs'
+import * as tf from "@tensorflow/tfjs"
 
 // Inject $fitLinear(xVals, yVals) in Vue, context and store.
 export default ({ app }, inject) => {
-  inject('fitLinear', (xVals, yVals) => {
+  inject("fitLinear", (xVals, yVals) => {
     // initial random values
     const M = tf.variable(tf.scalar(Math.random()))
     const B = tf.variable(tf.scalar(Math.random()))
     // optimizer constants
     const LEARNING_RATE = 0.5
     const OPTIMIZER = tf.train.sgd(LEARNING_RATE)
-    const ITERATIONS = process.env.ITERATIONS || 1000
+    const ITERATIONS = process.env.ITERATIONS || 10
     // predict function
-    const predict = (xVals) => M.mul(xVals).add(B)
-    const loss = (pred, label) => pred.sub(label).square().mean()
+    function predict(xVals) {
+      return tf.tidy(function () {
+        return M.mul(xVals).add(B)
+      })
+    }
+    function loss(pred, label) {
+      const error = pred.sub(label).square().mean()
+      return error
+    }
     // adjustment
     for (let j = 0; j < ITERATIONS; j++) {
       OPTIMIZER.minimize(() => loss(predict(xVals), yVals))
